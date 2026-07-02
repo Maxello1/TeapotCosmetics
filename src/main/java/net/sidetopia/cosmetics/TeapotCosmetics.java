f8095929ce0ca0edee8c931df00b17b37be53022
Maximillian Schoch <maximillian.schoch@gmail.com>
2026-07-02 22:50:27 +0200

    Initial commit for TeapotCosmetics with Small Crown

---
+++ src/main/java/net/sidetopia/cosmetics/TeapotCosmetics.java
@@ -0,0 +1,19 @@
+package net.sidetopia.cosmetics;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TeapotCosmetics implements ModInitializer {
+
+    public static final String MOD_ID = "teapot_cosmetics";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("[TeapotCosmetics] Initializing...");
+
+        // Tell Polymer to include our mod's custom assets in its auto-generated resource pack!
+        eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils.addModAssets(MOD_ID);
+    }
+}
