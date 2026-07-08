d83da2bd0fdab621bac307958d94d3b8cccea5f2
Maximillian Schoch <maximillian.schoch@gmail.com>
2026-07-09 01:24:00 +0200

    Add sgui dependency and Gem Table GUI (plain chest for now)

--- src/main/java/net/sidetopia/cosmetics/TeapotCosmetics.java
+++ src/main/java/net/sidetopia/cosmetics/TeapotCosmetics.java
@@ -1,6 +1,12 @@
 package net.sidetopia.cosmetics;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+import net.sidetopia.cosmetics.gui.GemTableGui;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,5 +21,23 @@ public class TeapotCosmetics implements ModInitializer {
 
         // Tell Polymer to include our mod's custom assets in its auto-generated resource pack!
         eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils.addModAssets(MOD_ID);
+
+        // Open Gem Table GUI when player right-clicks the gem_table block
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+            if (world.isClient()) return ActionResult.PASS;
+
+            var blockState = world.getBlockState(hitResult.getBlockPos());
+            var blockId = Registries.BLOCK.getId(blockState.getBlock());
+
+            // Check if the block is our gem_table (Filament registers it under teapot_cosmetics namespace)
+            if (blockId.equals(Identifier.of(MOD_ID, "gem_table"))) {
+                if (player instanceof ServerPlayerEntity serverPlayer) {
+                    new GemTableGui(serverPlayer).open();
+                    return ActionResult.SUCCESS;
+                }
+            }
+
+            return ActionResult.PASS;
+        });
     }
 }
---
+++ src/main/java/net/sidetopia/cosmetics/gui/GemTableGui.java
@@ -0,0 +1,15 @@
+package net.sidetopia.cosmetics.gui;
+
+import eu.pb4.sgui.api.gui.SimpleGui;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+
+public class GemTableGui extends SimpleGui {
+
+    public GemTableGui(ServerPlayerEntity player) {
+        // 6 rows chest GUI - can be replaced with a custom layout later
+        super(ScreenHandlerType.GENERIC_9X6, player, false);
+        this.setTitle(Text.literal("Gem Table"));
+    }
+}
